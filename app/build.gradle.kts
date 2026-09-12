96c7a44707306d84f18387c214d0bbf534503b32
Rabbi Hossain <rabbihossainlhp@gmail.com>
2026-09-12 07:06:37 +0600

    fixe plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.material)
     implementation(libs.kotlinx.coroutines.android)
 
     implementation(libs.retrofit)
